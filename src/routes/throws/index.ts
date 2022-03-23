89d9ac24b819ac3707c29cd45a4be6bf2307ddba
Christopher Carson <chris@nowzoo.com>
2022-03-23 15:43:57 -0500

    error message

--- src/routes/throws/index.ts
+++ src/routes/throws/index.ts
@@ -1,7 +1,7 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const get: RequestHandler = (event) => {
-  throw new Error('This always throws this error from this endpoint.')
+  throw new Error('My special endpoint error.')
 };
 
 
