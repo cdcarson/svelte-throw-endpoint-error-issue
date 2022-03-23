0ddbd43286e2b21637c03c5dafebc90fe5d0ef06
Christopher Carson <chris@nowzoo.com>
2022-03-23 15:46:30 -0500

    better link label

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<a href="/throws">Always throws</a>
+<a href="/throws">Route where the endpoint always throws</a>
